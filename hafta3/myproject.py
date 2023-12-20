9e4c23f8319587ffaa3b10d893bda936e3235e8c
UfukCanYuksel <ufkcnyksl62@gmail.com>
2023-12-20 10:40:34 +0000

    Odev 3

---
+++ hafta3/myproject.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
+import pandas as pd
+import requests
+
+app = Flask(__name__)
+api = Api(app)
+
+class GetAllData(Resource):
+    def get(self):
+
+        url = 'https://cat-fact.herokuapp.com/facts'     
+
+        resp = requests.get(url)
+        data = resp.json()
+
+
+        return {'data': data}, 200
+    
+api.add_resource(GetAllData, '/facts')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
+    app.run()
